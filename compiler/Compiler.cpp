099cdccf49173ac504a73d0e4b387f59b6e2a283
May the source be with you <misa@Misas-iMac.local>
2017-10-07 17:06:09 +0800

    missed include for linux

--- compiler/Compiler.cpp
+++ compiler/Compiler.cpp
@@ -3,6 +3,8 @@
 #include <assert.h>
 #include <sstream>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 static unsigned char cmap[256]={
 	//+0 +1 +2 +3 +4 +5 +6 +7 +8 +9 +A +B +C +D +E +F
