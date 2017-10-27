1dc9b5b26418d18961889fdb3d49d3dc4df4253b
May the source be with you <misa@Misas-iMac.local>
2017-10-27 21:11:08 +0800

    js shell

--- jsh.cpp
+++ jsh.cpp
@@ -9,33 +9,33 @@ void _moduleLoader(CTinyJS* tinyJS);
 
 //show help message
 void help() {
-	printf("\033[4m\033[0;33m[Help] JSM Javascript engine, you can run javascript file directly like \"jsh [file]\", or input javascript code here.\n"
+	printf("\033[4m\033[0;33m[Help] JSM Javascript engine, you can input javascript code here.\n"
 			"commands:\n"
 			"\thelp       : show this help.\n"
 			"\tbc         : compiler the whole javascript to bytecode.\n"
-			"\tbc  [file] : compiler file to bytecode. will reset runtime.\n"
-			"\treset      : reset runtime, clear all variable and stack.\n"
-			"\trun        : run the new input javascript.\n"
-			"\trun [file] : run file, will reset runtime first.\n"
+			"\trun        : run the new input javascript, will reset runtime first.\n"
 			"\texit       : quit shell.\033[0m\n");
 }
 
 //js shell
 void jshell() {
 	std::string input;
-	std::string run;
 	CTinyJS tinyJS;
 	tinyJS.loadModule(_moduleLoader);
 	help();
+	printf("\033[4m\033[0;32mjsh:>\033[0m ");
 
+	bool prompt = true;
 	while(true) {
-		printf("\033[4m\033[0;32mjsh:>\033[0m ");
 		char buffer[2048+1];
 		if(fgets ( buffer, 2048, stdin ) == NULL)
 			break;
-
+			
 		string ln = buffer;
 		ln = StringUtil::trim(ln);
+		if(ln.length() == 0) {
+			prompt = !prompt;
+		}
 
 		if(ln == "help") {
 			help();
@@ -44,27 +44,13 @@ void jshell() {
 			printf("\n\033[4m\033[0;33m[Bye!]\033[0m\n");
 			break;
 		}
-		else if(ln == "reset") {
-			tinyJS.reset();
-			input = "";
-			run = "";
-		}
 		else if(ln == "run") {
 			try {
-				tinyJS.exec(run);
-			} catch (CScriptException *e) {
-				printf("ERROR: %s\n", e->text.c_str());
-			}
-			run = "";
-		}
-		else if(ln.find("run ") != string::npos) {
-			try {
 				tinyJS.reset();
-				tinyJS.run(ln.substr(4));
+				tinyJS.exec(input);
 			} catch (CScriptException *e) {
 				printf("ERROR: %s\n", e->text.c_str());
 			}
-			run = "";
 			input = "";
 		}
 		else if(ln == "bc") {
@@ -77,22 +63,13 @@ void jshell() {
 				printf("ERROR: %s\n", e->text.c_str());
 			}
 		}
-		else if(ln.find("bc ") != string::npos) {
-			Compiler compiler;
-			try {
-				compiler.run(ln.substr(3));
-				string s = compiler.bytecode.dump();
-				printf("%s\n", s.c_str());
-			} catch (CScriptException *e) {
-				printf("ERROR: %s\n", e->text.c_str());
-			}
-			run = "";
-			input = "";
+		else if(ln.length() > 0) {
+			input += ln + "\n";
+			prompt = false;
 		}
-		else {
-			run += ln;
-			input += ln;
-		}
+
+		if(prompt)
+			printf("\033[4m\033[0;32mjsh:>\033[0m ");
 	}	
 }
 
