d26de293f689b7e8f1bc800e8b83e5a0c4550906
May the source be with you <misa.zhu@gmail.com>
2017-11-13 21:25:49 +0800

    Thread

---
+++ native/Thread/Thread.cpp
@@ -0,0 +1,76 @@
+#include "Thread.h"
+##include "utils/Thread/Thread.h"
+//#include "utils/File/File.h"
+#include <cstdlib>
+#include <unistd.h>
+
+using namespace std;
+using namespace JSM;
+
+typedef struct ThreadData {
+	KoalaJS* js;
+	std::string src;
+	bool code;
+	BCVar* arg;
+} ThreadDataT;
+
+static void* _vmThread(void* data) {
+	ThreadDataT* td = (ThreadDataT*)data;
+	if(td == NULL)
+		return NULL;
+
+	KoalaJS* js = td->js;
+	std::string src = td->src;
+	bool code = td->code;
+	BCVar* arg = td->arg;
+
+	delete td;
+	pthread_detach(pthread_self());
+
+	KoalaJS *tJS = new KoalaJS();
+	tJS->loadModule(js->getModuleLoader());
+	tJS->setcwd(js->getcwd());
+	tJS->getRoot()->addChild("_threadArg", arg);
+	arg->unref();
+
+	if(code)
+		tJS->exec(src);
+	else {
+		tJS->run(src);
+	}
+	
+	delete tJS;
+	return NULL;
+}
+
+void JSThread::exec(KoalaJS* js, BCVar *c, void *userdata) {
+	ThreadDataT *data = new ThreadDataT();
+
+	data->js = js;
+	data->src = c->getParameter("src")->getString();
+	data->code = true;
+	data->arg = c->getParameter("arg")->ref();
+
+	Thread::run(_vmThread, data);
+}
+
+void JSThread::run(KoalaJS* js, BCVar *c, void *userdata) {
+	ThreadDataT *data = new ThreadDataT();
+
+	data->js = js;
+	data->src = c->getParameter("file")->getString();
+	data->code = false;
+	data->arg = c->getParameter("arg")->ref();
+	Thread::run(_vmThread, data);
+}
+
+void JSThread::sleep(KoalaJS* js, BCVar *c, void *userdata) {
+	int sec = c->getParameter("sec")->getInt();
+	::sleep(sec);
+}
+
+void JSThread::usleep(KoalaJS* js, BCVar *c, void *userdata) {
+	int sec = c->getParameter("usec")->getInt();
+	::usleep(sec);
+}
+
